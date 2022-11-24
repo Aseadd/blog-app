35637313ac90afbe632d1fb66cf8098061925bb7
addis <betsega23@gmail.com>
2022-11-25 02:23:04 +0300

    Add changes

--- db/migrate/20221124153513_create_comment.rb
+++ db/migrate/20221124153513_create_comment.rb
@@ -1,12 +1,9 @@
 class CreateComment < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.string :author_id
-      t.string :post_id
-        
-      end
       t.text :text
 
       t.timestamps
     end
   end
+end
--- db/migrate/20221124153553_create_like.rb
+++ db/migrate/20221124153553_create_like.rb
@@ -1,10 +1,8 @@
 class CreateLike < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.string :author_id
-      t.string :post_id
 
       t.timestamps
     end
-  end
+ end
 end
