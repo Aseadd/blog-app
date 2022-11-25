0313d5b6a473091b62ccc4a1e56bd90792f25a69
addis <betsega23@gmail.com>
2022-11-25 15:59:36 +0300

    Add Comment model and its methods

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    belongs_to :post, counter_cache: :comments_counter
+
+    def update_counters
+        update(comments_counter: comments.count)
+    end
+end
