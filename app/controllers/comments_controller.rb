0c08e60d7c57b957612c0e90dabcff023ee4dd9d
addis <betsega23@gmail.com>
2022-12-07 19:56:12 +0300

    Update other tests

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,19 +2,19 @@ class CommentsController < ApplicationController
   def new
     comment = Comment.new
     respond_to do |format|
-      format.html { render :new, locals: { comment: } }
+      format.html { render :new, locals: { comment: :comment} }
     end
   end
 
   def create
     user = User.find(params[:user_id])
     post = Post.find(params[:post_id])
-    comment = Comment.new(text: params[:comment][:text], post:, author: current_user)
+    comment = Comment.new(text: params[:comment][:text], post: post, author: current_user)
     if comment.save
       redirect_to user_post_path(user, post)
     else
       flash.now[:error] = 'Error: comment could not be saved'
-      render :new, locals: { comment: }
+      render :new, locals: { comment: :comment}
     end
   end
 end
